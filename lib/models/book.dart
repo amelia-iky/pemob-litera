1d67b5966e2f505a5de11ffa84c1039722e1d7dd
amelia-iky <lliameliia.2212@gmail.com>
2025-06-25 03:20:47 +0800

    feat: add Book model with JSON serialization

--- lib/models/book.dart
+++ lib/models/book.dart
@@ -3,12 +3,16 @@ class Book {
   final String title;
   final String coverImage;
   final String category;
+  final String details;
+  final String publisher;
 
   Book({
     required this.id,
     required this.title,
     required this.coverImage,
     required this.category,
+    required this.details,
+    required this.publisher,
   });
 
   factory Book.fromJson(Map<String, dynamic> json) {
@@ -17,6 +21,10 @@ class Book {
       title: json['title'],
       coverImage: json['cover_image'],
       category: json['category']['name'],
+      details: 'Harga: ${json['details']['price']}, '
+               'Halaman: ${json['details']['total_pages']}, '
+               'Terbit: ${json['details']['published_date']}',
+      publisher: json['publisher'],
     );
   }
 }
