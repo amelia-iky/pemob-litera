df5a12001222b3b6035ebc0d369ce37aa359e72e
amelia-iky <lliameliia.2212@gmail.com>
2025-06-25 03:21:03 +0800

    refactor: update main app structure and replace BookStorePage with HomePage

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'pages/home_pages.dart';
 
 void main() {
   runApp(const MyApp());
@@ -6,57 +7,11 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Gramedia Book Store',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const BookStorePage(),
-    );
-  }
-}
-
-class BookStorePage extends StatelessWidget {
-  const BookStorePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Gramedia Book Store'),
-      ),
-      body: ListView.builder(
-        itemCount: 10, // 10 baris
-        itemBuilder: (context, rowIndex) {
-          return SizedBox(
-            height: MediaQuery.of(context).size.width / 2, // Tinggi item proporsional ke lebar layar
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: 3, // 3 item per baris
-              itemBuilder: (context, colIndex) {
-                double itemWidth = MediaQuery.of(context).size.width / 2.5; // Responsive width
-
-                return Container(
-                  width: itemWidth,
-                  margin: const EdgeInsets.all(8),
-                  decoration: BoxDecoration(
-                    color: Colors.blue[100 * ((rowIndex + colIndex) % 9)],
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: Center(
-                    child: Text(
-                      'Book ${rowIndex + 1} - ${colIndex + 1}',
-                      style: const TextStyle(fontSize: 16),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                );
-              },
-            ),
-          );
-        },
-      ),
+      title: 'Litera',
+      home: HomePage(),
     );
   }
 }
