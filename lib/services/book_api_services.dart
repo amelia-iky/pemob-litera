8632c5b4b6c5a7202fbf85144fd857b76a0d7b7b
amelia-iky <lliameliia.2212@gmail.com>
2025-06-25 07:36:44 +0800

    feat: implement searchBooks method in BookApiService for querying books

--- lib/services/book_api_services.dart
+++ lib/services/book_api_services.dart
@@ -5,16 +5,17 @@ import '../models/book_models.dart';
 class BookApiService {
   static const String baseUrl = 'https://bukuacak-9bdcb4ef2605.herokuapp.com/api/v1/book';
 
-  static Future<List<Book>> fetchBooks() async {
-    final response = await http.get(Uri.parse(baseUrl));
+  static Future<List<Book>> searchBooks(String query) async {
+    final response = await http.get(
+      Uri.parse('$baseUrl?keyword=$query'),
+    );
 
     if (response.statusCode == 200) {
       final data = jsonDecode(response.body);
       final List<dynamic> booksJson = data['books'];
-
       return booksJson.map((json) => Book.fromJson(json)).toList();
     } else {
-      throw Exception('Failed to load books');
+      throw Exception('Failed to search books');
     }
   }
 }
