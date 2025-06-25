a66d2f90e19407b00b6c3f2bd4dd4c90d97018e8
amelia-iky <lliameliia.2212@gmail.com>
2025-06-25 19:57:34 +0800

    feat: remove BookRepository class and its associated methods

--- lib/repositories/book_repository.dart
+++
@@ -1,8 +0,0 @@
-import '../models/book_models.dart';
-import '../services/book_api_services.dart';
-
-class BookRepository {
-  Future<List<Book>> getAllBooks() async {
-    return await BookApiService.fetchBooks();
-  }
-}
