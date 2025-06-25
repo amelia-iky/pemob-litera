9269217f656421c37ce6fd64e5a78063cf6e54c9
amelia-iky <lliameliia.2212@gmail.com>
2025-06-26 03:38:30 +0800

    refactor: simplify header formatting and improve code readability in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
@@ -66,9 +65,7 @@ class _SessionCheckerState extends State<SessionChecker> {
     try {
       final response = await http.get(
         Uri.parse('https://api-litera.vercel.app/auth/keep-login'),
-        headers: {
-          'Authorization': 'Bearer $token',
-        },
+        headers: {'Authorization': 'Bearer $token'},
       );
 
       if (response.statusCode == 200) {
@@ -90,9 +87,7 @@ class _SessionCheckerState extends State<SessionChecker> {
   @override
   Widget build(BuildContext context) {
     if (_isLoading) {
-      return const Scaffold(
-        body: Center(child: CircularProgressIndicator()),
-      );
+      return const Scaffold(body: Center(child: CircularProgressIndicator()));
     }
 
     return _isLoggedIn ? const HomePage() : const LoginPage();
