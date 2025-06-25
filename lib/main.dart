472dbf6d9881a396fdd46116dd401b979d46339c
amelia-iky <lliameliia.2212@gmail.com>
2025-06-26 03:52:47 +0800

    refactor: remove unnecessary blank line in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
-
 import 'pages/login_pages.dart';
 import 'pages/home_pages.dart';
 
