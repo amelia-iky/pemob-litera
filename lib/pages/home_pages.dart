5a0d654f913cea496f4c6f109b5c8db0d720c08b
amelia-iky <lliameliia.2212@gmail.com>
2025-06-28 19:00:19 +0800

    feat: refactor HomePage to use CustomDrawer and BookSearch widgets, improving code organization and readability

--- lib/pages/home_pages.dart
+++ lib/pages/home_pages.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
-import './profile_pages.dart';
 import '../models/book_models.dart';
 import '../models/genre_models.dart';
 import '../models/user_models.dart';
 import '../services/book_api_service.dart';
-import '../services/genre_api_service.dart';
 import '../services/user_api_service.dart';
 import '../widgets/book_card.dart';
 import '../widgets/book_category.dart';
+import '../widgets/book_search.dart';
+import '../widgets/custom_drawer.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -27,13 +27,14 @@ class _HomePageState extends State<HomePage> {
   int currentPage = 0;
   final int genresPerPage = 5;
 
-  late Future<UserProfile> _userProfile;
+  Future<UserProfile> _fetchUserProfile() {
+    return UserApiService.fetchUserProfile();
+  }
 
   @override
   void initState() {
     super.initState();
-    _genreFuture = GenreApiService.fetchGenreStats();
-    _userProfile = UserApiService.fetchUserProfile();
+    _genreFuture = BookApiService.fetchGenreStats();
   }
 
   void _performSearch(String query) {
@@ -58,191 +59,28 @@ class _HomePageState extends State<HomePage> {
         centerTitle: true,
         title: const Text(
           'Litera',
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-          ),
+          style: TextStyle(fontSize: 22, fontWeight: FontWeight.w800),
         ),
         backgroundColor: const Color(0xfff8c9d3),
       ),
-      drawer: Drawer(
-        child: Column(
-          children: [
-            FutureBuilder<UserProfile>(
-              future: _userProfile,
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const SizedBox(
-                    height: 200,
-                    width: double.infinity,
-                    child: DrawerHeader(
-                      decoration: BoxDecoration(color: Color(0xfff8c9d3)),
-                      margin: EdgeInsets.zero,
-                      padding: EdgeInsets.zero,
-                      child: Center(
-                        child: CircularProgressIndicator(color: Colors.white),
-                      ),
-                    ),
-                  );
-                } else if (snapshot.hasError || !snapshot.hasData) {
-                  return const SizedBox(
-                    height: 200,
-                    width: double.infinity,
-                    child: DrawerHeader(
-                      decoration: BoxDecoration(color: Color(0xfff8c9d3)),
-                      margin: EdgeInsets.zero,
-                      padding: EdgeInsets.zero,
-                      child: Center(
-                        child: Text(
-                          'User tidak ditemukan',
-                          style: TextStyle(color: Colors.white, fontSize: 18),
-                        ),
-                      ),
-                    ),
-                  );
-                }
-
-                final user = snapshot.data!;
-                final imageUrl = user.profileImages.isNotEmpty
-                    ? user.profileImages.first.url
-                    : null;
-
-                return SizedBox(
-                  height: 200,
-                  width: double.infinity,
-                  child: DrawerHeader(
-                    decoration: const BoxDecoration(color: Color(0xfff8c9d3)),
-                    margin: EdgeInsets.zero,
-                    padding: const EdgeInsets.symmetric(horizontal: 16),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        CircleAvatar(
-                          radius: 35,
-                          backgroundColor: Colors.white,
-                          backgroundImage: imageUrl != null
-                              ? NetworkImage(imageUrl)
-                              : null,
-                          child: imageUrl == null
-                              ? const Icon(
-                                  Icons.person,
-                                  size: 50,
-                                  color: Colors.grey,
-                                )
-                              : null,
-                        ),
-                        const SizedBox(height: 12),
-                        Text(
-                          user.name,
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis,
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Text(
-                          user.email,
-                          overflow: TextOverflow.ellipsis,
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 14,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                );
-              },
-            ),
-            Expanded(
-              child: ListView(
-                padding: EdgeInsets.zero,
-                children: [
-                  ListTile(
-                    leading: const Icon(Icons.person),
-                    title: const Text('Profile'),
-                    onTap: () async {
-                      Navigator.pop(context);
-
-                      final updated = await Navigator.push<bool>(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const ProfilePage(),
-                        ),
-                      );
-
-                      if (updated == true) {
-                        setState(() {
-                          _userProfile = UserApiService.fetchUserProfile();
-                        });
-                      }
-
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const ProfilePage(),
-                        ),
-                      );
-                    },
-                  ),
-                  const ListTile(
-                    leading: Icon(Icons.favorite),
-                    title: Text('Favorite'),
-                  ),
-                  const ListTile(
-                    leading: Icon(Icons.bookmark),
-                    title: Text('Saved'),
-                  ),
-                ],
-              ),
-            ),
-            const Padding(
-              padding: EdgeInsets.only(top: 16, bottom: 32),
-              child: Column(
-                children: [
-                  Text(
-                    'Developed with ❤️ by Amelia Rizky Yuniar',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 13, color: Colors.grey),
-                  ),
-                  SizedBox(height: 4),
-                  Text(
-                    'Version 1.0.0',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 13, color: Colors.grey),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
+      drawer: CustomDrawer(
+        fetchUserProfile: _fetchUserProfile,
+        onProfileUpdated: () {
+          setState(() {
+            // _fetchUserProfile = UserApiService.fetchUserProfile();
+          });
+        },
       ),
       body: Column(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(12),
-            child: TextField(
-              controller: _searchController,
-              decoration: InputDecoration(
-                hintText: 'Cari buku...',
-                prefixIcon: const Icon(Icons.search),
-                suffixIcon: IconButton(
-                  icon: const Icon(Icons.clear),
-                  onPressed: () {
-                    _searchController.clear();
-                    _performSearch('');
-                  },
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
-                ),
-              ),
-              onSubmitted: _performSearch,
-            ),
+          // Search Bar
+          BookSearch(
+            controller: _searchController,
+            onSearch: _performSearch,
+            onClear: () {
+              _searchController.clear();
+              _performSearch('');
+            },
           ),
           Expanded(
             child: _isSearching && _searchResults != null
@@ -292,13 +130,6 @@ class _HomePageState extends State<HomePage> {
                         return const Center(child: Text('No genres found.'));
                       }
 
-                      final genres = genreSnapshot.data!;
-                      final totalPages = (genres.length / genresPerPage).ceil();
-                      final pagedGenres = genres
-                          .skip(currentPage * genresPerPage)
-                          .take(genresPerPage)
-                          .toList();
-
                       return Column(
                         children: [
                           Expanded(
@@ -331,7 +162,8 @@ class _HomePageState extends State<HomePage> {
                                     .take(genresPerPage)
                                     .toList();
 
-                                return GenreBookList(
+                                // Book Category
+                                return BookCategory(
                                   pagedGenres: pagedGenres,
                                   currentPage: currentPage,
                                   totalPages: totalPages,
