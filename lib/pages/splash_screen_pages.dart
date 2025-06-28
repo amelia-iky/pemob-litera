9733066cd73c31679dca282dcca9055b376e1e99
amelia-iky <lliameliia.2212@gmail.com>
2025-06-28 16:56:23 +0800

    feat: add SplashScreen widget for app launch with a 2-second delay to sign-in page

---
+++ lib/pages/splash_screen_pages.dart
@@ -0,0 +1,31 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'signin_pages.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => SignInPage()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Image.asset('assets/icon/app_icon.png', width: 150, height: 150),
+      ),
+    );
+  }
+}
